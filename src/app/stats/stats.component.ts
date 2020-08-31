cffad4a19f5da4a7a29e198569dc9889cab35a0d
sadblondeboi <bezawka@gmail.com>
2020-08-31 19:46:14 +0200

    Ip changes

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,24 +1,18 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Player } from '../models/player';
-
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
-  styleUrls: ['./stats.component.scss']
+  styleUrls: ['./stats.component.scss'],
 })
-
-export class StatsComponent implements OnInit {
+export class StatsComponent {
   @Input()
   public players: {
-    player1: Player,
-    player2: Player
+    player1: Player;
+    player2: Player;
   };
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
